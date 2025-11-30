394d21f002ab2e7872b3e1391d7e365536678461
luisbossa <luisbossa@gmail.com>
2025-11-29 21:19:49 -0600

    better website

--- js/layout.js
+++ js/layout.js
@@ -3,9 +3,6 @@ document.addEventListener("DOMContentLoaded", () => {
   if (!footer) return;
 
   footer.innerHTML = `
-    <div class="footer-link">
-      © 2025 100Puntadas. Website creado por 
-      <a href="https://bstudio.site/" target="_blank" class="link">BStudio</a>
-    </div>
+     
   `;
 });
