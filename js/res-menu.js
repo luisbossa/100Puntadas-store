393cfbe71f1d26da42149873ad1b77ec1ed9f34c
luisbossa <luisbossa@gmail.com>
2025-12-03 07:48:32 -0600

    no scroll

--- js/res-menu.js
+++ js/res-menu.js
@@ -2,19 +2,19 @@ const hamburger = document.getElementById("hamburger");
 const mobileMenu = document.getElementById("mobileMenu");
 const closeMenuBtn = document.getElementById("closeMenuBtn");
 
-// abrir menú
 hamburger.addEventListener("click", () => {
   mobileMenu.classList.add("active");
+  document.body.classList.add("no-scroll");
 });
 
-// cerrar menú
 closeMenuBtn.addEventListener("click", () => {
   mobileMenu.classList.remove("active");
+  document.body.classList.remove("no-scroll");
 });
 
-// cerrar cuando se cliquea un link
 document.querySelectorAll(".mobile-link").forEach((link) => {
   link.addEventListener("click", () => {
     mobileMenu.classList.remove("active");
+    document.body.classList.remove("no-scroll");
   });
 });
