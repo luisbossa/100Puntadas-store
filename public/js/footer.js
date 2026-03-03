a0390115e744a01aa8478b3c90a9fc4ff54a10c3
luisbossa <luisbossa@gmail.com>
2026-03-03 01:36:39 -0600

    footer

--- public/js/footer.js
+++ public/js/footer.js
@@ -1,33 +1,39 @@
-$(".footer-anim-wrapper").each(function (index) {
-  const targets = $(this).add($(".footer-wrapper"));
+gsap.registerPlugin(ScrollTrigger);
 
-  let tl = gsap.timeline({
-    scrollTrigger: {
-      id: "navScrolltrigger",
-      trigger: $(this),          
-      start: "top bottom",
-      end: "bottom 70%",
-      scrub: true,
-    },
+let mm = gsap.matchMedia();
+
+mm.add("(min-width: 992px)", () => {
+  $(".footer-anim-wrapper").each(function (index) {
+    const targets = $(this).add($(".footer-wrapper"));
+
+    let tl = gsap.timeline({
+      scrollTrigger: {
+        id: "navScrolltrigger-" + index,
+        trigger: $(this),
+        start: "top bottom",
+        end: "bottom 70%",
+        scrub: true,
+      },
+    });
+
+    tl.from(targets.find(".page-padding"), {
+      yPercent: 50,
+    });
+
+    tl.from(
+      targets.find(".logo-footer-wrapper img"),
+      {
+        yPercent: 20,
+      },
+      "<",
+    );
+
+    tl.from(
+      targets.find(".page-padding"),
+      {
+        opacity: 0,
+      },
+      "<0.1",
+    );
   });
-
-  tl.from(targets.find(".page-padding"), {
-    yPercent: 50,
-  });
-
-  tl.from(
-    targets.find(".logo-footer-wrapper img"),
-    {
-      yPercent: 20,
-    },
-    "<"
-  );
-
-  tl.from(
-    targets.find(".page-padding"),
-    {
-      opacity: 0,
-    },
-    "<0.1"
-  );
 });
